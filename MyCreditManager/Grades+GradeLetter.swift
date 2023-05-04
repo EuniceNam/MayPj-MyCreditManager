7b4243b664f70cee382ed690d26945d6c7381b16
EuniceNam <caellime@gmail.com>
2023-05-04 19:51:05 +0900

    구조

---
+++ MyCreditManager/Grades+GradeLetter.swift
@@ -0,0 +1,47 @@
+//
+//  Grades+GradeLetter.swift
+//  MyCreditManager
+//
+//  Created by Hyorim Nam on 2023/04/27.
+//
+
+import Foundation
+
+struct Grades {
+    var grades: [Grade]
+    func calcGPA() -> Float {
+        return grades.map({GradeLetter[$0.grade]}).reduce(0.0, +) / Float(grades.count)
+    }
+}
+
+enum GradeLetter: String {
+    // swiftlint: disable identifier_name
+    case A_plus = "A+"
+    case A_zero = "A"
+    case B_plus = "B+"
+    case B_zero = "B"
+    case C_plus = "C+"
+    case C_zero = "C"
+    case D_plus = "D+"
+    case D_zero = "D"
+    case F      = "F"
+    // swiftlint: enable identifier_name
+
+    static subscript(_ gradeString: String) -> Self? {
+        return Self(rawValue: gradeString)
+    }
+
+    static subscript(_ gradeLetter: Self) -> Float {
+        switch gradeLetter {
+        case .A_plus: return 4.5
+        case .A_zero: return 4.0
+        case .B_plus: return 3.5
+        case .B_zero: return 3.0
+        case .C_plus: return 2.5
+        case .C_zero: return 2.0
+        case .D_plus: return 1.5
+        case .D_zero: return 1.0
+        case .F:      return 0.0
+        }
+    }
+}
--- MyCreditManager/main.swift
+++ MyCreditManager/main.swift
@@ -7,5 +7,63 @@
 
 import Foundation
 
-print("Hello, World!")
+typealias StudentRecord = [String: Grades]
+typealias Grade = (subject: String, grade: GradeLetter)
 
+// - MARK: main
+
+var students: StudentRecord = [:]
+
+menuLoop: while true {
+    print("메뉴")
+    if let menuInput = readLine() {
+        switch (MenuSelection[menuInput.trimmingCharacters(in: .whitespaces)]) {
+        case .createStudent:
+            print("학생추가")
+            if let studentName = readLine() {
+                createStudent(studentName)
+            } else {
+                print("")
+            }
+        case .removeStudent:
+            print("학생삭제")
+        case .updateGrade:
+            print("성적 추가/변경")
+        case .deleteGrade:
+            print("성적 삭제")
+        case .printGPA:
+            print("평균성적 출력")
+            print(String(format: "%.2f"), 123.342)
+        case .exit:
+            break menuLoop
+        default:
+            print("입력오류")
+            
+        }
+    }
+}
+
+// - MARK: functions
+
+func createStudent(_ name: String) {
+    
+}
+func removeStudent(_ name: String) {
+    
+}
+
+// - MARK: help enumerations
+
+enum MenuSelection: Int {
+    case createStudent = 1
+    case removeStudent
+    case updateGrade
+    case deleteGrade
+    case printGPA
+    case exit
+
+    static subscript(_ menuString: String) -> Self? {
+        guard let menuSelection = Int(menuString) else {return nil}
+        return Self(rawValue: menuSelection)
+    }
+}
